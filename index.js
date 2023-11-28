96d456911ee3f70a1b47d4e43461d6c4bbe3eaf8
Manipriya <manipriyagopal1999@gmail.com>
2023-11-28 21:14:24 +0530

    Implemented bullet firing

--- index.js
+++ index.js
@@ -1,64 +1,129 @@
+// window.onload = function () {
+//   let canvas = document.getElementById("canvas");
+//   let context = canvas.getContext("2d");
+//   let width = (canvas.width = window.innerWidth);
+//   let height = (canvas.height = window.innerHeight);
+//   let mouseX = 0;
+//   let mouseY = 0;
+//   let lineY = height / 2; //vertically starting point of line
+//   let lineLength = 40;
+//   let line = new Line(lineY, lineLength);
+//   let sample = new Particle(10); // This creates a Particle with initial velocities (0, 0)
+//   // sample.acceleration.setX(0);
+//   update();
+
+//   function update() {
+//     context.clearRect(0, 0, width, height);
+//     line.update(mouseX, mouseY);
+   
+//   sample.update();
+
+//     sample.draw(context);
+//     line.draw(context);
+
+//     requestAnimationFrame(update);
+//   }
+//   window.addEventListener("mousemove", function (event) {
+//     mouseX = event.clientX;
+//     mouseY = event.clientY;
+//   });
+  
+// };
+
+// window.onload = function () {
+//   let canvas = document.getElementById("canvas");
+//   let context = canvas.getContext("2d");
+//   let width = (canvas.width = window.innerWidth);
+//   let height = (canvas.height = window.innerHeight);
+//     let mouseX = 0;
+//   let mouseY = 0;
+//   let lineY = height / 2;
+//   let lineLength = 40;
+//   let line = new Line(lineY, lineLength);
+//   let sample = new Particle(10, width / 2, height / 2);
+//   let isMouseDown = true;
+
+//   update();
+
+//   function update() {
+//     context.clearRect(0, 0, width, height);
+//     line.update(mouseX, mouseY);
+
+//     sample.update();
+
+//     if (isMouseDown) {
+//       sample.draw(context);
+//     }
+//     line.draw(context)
+
+//     requestAnimationFrame(update);
+//   }
+//   window.addEventListener("mousemove", function (event) {
+//     mouseX = event.clientX;
+//     mouseY = event.clientY;
+//   });
+  
+//   window.addEventListener("mousedown", function (event) {
+//     const angle = Math.atan2(event.clientY - sample.position.getY(), event.clientX - sample.position.getX());
+//     const speed = 25;
+//     sample.velocity.setAngle(angle).setMagnitude(speed);
+//     isMouseDown = true;
+//   });
+
+//   window.addEventListener("mouseup", function () {
+//     isMouseDown = false;
+//   });
+// };
+
 window.onload = function () {
-    let canvas = document.getElementById("canvas");
-    let context = canvas.getContext("2d");
-    let width = (canvas.width = window.innerWidth);
-    let height = (canvas.height = window.innerHeight);
-    let mouseX = 0;
-    let mouseY = 0;
-    let lineY = height / 2; //vertically starting point of line
-    let lineLength = 40;
-    let line = new Line(lineY, lineLength);
-    let sample = new Particle(0, 0, 10);
-    sample.acceleration.setX(2);
-    update();
-  
-    function update() {
-      context.clearRect(0, 0, width, height);
-      line.update(mouseX, mouseY);
-      sample.update();
-  
+  let canvas = document.getElementById("canvas");
+  let context = canvas.getContext("2d");
+  let width = (canvas.width = window.innerWidth);
+  let height = (canvas.height = window.innerHeight);
+  let mouseX = 0;
+  let mouseY = 0;
+  let lineY = height / 2;
+  let lineLength = 40;
+  let line = new Line(lineY, lineLength);
+  let sample = new Particle(10, width / 2, height / 2);
+  let isMouseDown = true;
+
+  update();
+
+  function update() {
+    context.clearRect(0, 0, width, height);
+    line.update(mouseX, mouseY);
+
+    sample.update();
+
+    if (isMouseDown) {
       sample.draw(context);
-      line.draw(context);
-  
-      requestAnimationFrame(update);
     }
-    window.addEventListener("mousemove", function (event) {
-      mouseX = event.clientX;
-      mouseY = event.clientY;
-    });
-  };
-  
-  // window.onload = function () {
-  //   let canvas = document.getElementById("canvas");
-  //   let context = canvas.getContext("2d");
-  //   let width = (canvas.width = window.innerWidth);
-  //   let height = (canvas.height = window.innerHeight);
-  
-  //   let sample = new Particle(0, 0, 10);
-  //   sample.acceleration.setX(2);
-  
-  //   let rectangle = new Rectangle(width / 2 - 50, height / 2 - 25, 100, 50);
-  
-  //   update();
-  
-  //   function update() {
-  //     context.clearRect(0, 0, width, height);
-  
-  //     sample.update();
-  //     rectangle.update();
-  
-  //     sample.draw(context);
-  //     rectangle.draw(context);
-  
-  //     requestAnimationFrame(update);
-  //   }
-  
-  //   // Mouse move event listener
-  //   window.addEventListener("mousemove", function (event) {
-  //     // Update the rectangle's position based on mouse movement
-  //     rectangle.setPosition(
-  //       event.clientX - rectangle.width / 2,
-  //       event.clientY - rectangle.height / 2
-  //     );
-  //   });
-  // };
+    line.draw(context);
+
+    requestAnimationFrame(update);
+  }
+
+  window.addEventListener("mousemove", function (event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+  });
+
+  window.addEventListener("mousedown", function (event) {
+    // Reset particle position to the center
+    sample.position.setX(width / 2);
+    sample.position.setY(height / 2);
+
+    const angle = Math.atan2(
+      event.clientY - sample.position.getY(),
+      event.clientX - sample.position.getX()
+    );
+    const speed = 100;
+    sample.velocity.setAngle(angle).setMagnitude(speed);
+    isMouseDown = true;
+  });
+
+  window.addEventListener("mouseup", function () {
+    isMouseDown = false;
+  });
+};
