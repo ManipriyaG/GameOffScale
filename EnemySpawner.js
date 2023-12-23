2b01e19d584482eb661076b43907db78504d295a
kavinsaiyan <37509336+kavinsaiyan@users.noreply.github.com>
2023-12-23 18:09:45 +0530

    Update EnemySpawner.js

--- EnemySpawner.js
+++ EnemySpawner.js
@@ -13,7 +13,7 @@ class EnemySpawning {
       const config = this.spawnConfigurations[this.currentConfigIndex];
     
       // Check if the current time matches the spawn time for this configuration
-      if (currentTime / 1000 >= config.spawnTime) {
+      if (currentTime / 1000 >= config.spawnTime && this.currentConfigIndex < spawnConfigurations.Length) {
         // Spawn the specified number of enemies at the specified location with the target
         this.enemySystem.spawnEnemies(
           config.initialX,
@@ -26,7 +26,7 @@ class EnemySpawning {
         console.log(`Enemies spawned at ${currentTime / 1000} seconds!`);
     
         this.lastSpawnTime = currentTime;
-        this.currentConfigIndex = (this.currentConfigIndex + 1) % this.spawnConfigurations.length;
+        this.currentConfigIndex = this.currentConfigIndex + 1;
       } else {
         console.log(`Waiting for spawn time (${config.spawnTime} seconds)...`);
       }
